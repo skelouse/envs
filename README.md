eb8e071eee510a9491b6e27b48c66cf545992efd
Samuel Stoltenberg <sam@skelouse.com>
2023-12-04 18:24:37 -0600

    init

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
 # envs
+
+This is my git-friendly manager for handling Python virtual environments. It lets you easily create, activate, and update your environments. It keeps track of package dependencies with `venv-freeze` using `$VENV_DIR/<env_name>/requirements.txt`.
+
+## Examples
+
+- Creating a new environment: `venv-create newenv`
+- Activating an environment: `venv-activate myenv`
+- Freezing an environment's requirements: `venv-freeze myenv`
+- Initializing all environments on another system: `venv-init`
+
+## Setup
+
+- Fork the repository by visiting: https://github.com/skelouse/envs/fork
+
+- Clone your forked repository. 
+This command will clone the repository into the default directory: `$HOME/envs`
+
+```bash
+git clone git@github.com:<your_username>/envs.git $HOME/envs
+```
+
+- Add this line to your shell configuration file (.bashrc, .zshrc, etc.):
+
+```
+source "$HOME/envs/venv.sh"
+```
+
+## Scripts
+
+### venv.sh
+
+This script provides several functions to manage python virtual environments:
+
+- `venv-activate <env_name>`: Activates the specified virtual environment.
+- `venv-freeze [env_name]`: Freezes the current environment's packages into a `requirements.txt` file. If no environment is specified, it uses the currently active one.
+- `venv-create <env_name>`: Creates a new virtual environment with the specified name.
+- `venv-init`: After a clone will iterate environments `$VENV_DIR/<env_name/requirements.txt` and install the dependencies for those environments
+
+- After venv-freeze, track the changes with Git:
+  ```bash
+  cd "$VENV_DIR/<env_name>"
+  git add requirements.txt
+  git commit -m "Update dependencies for <env_name>"
+  git push origin master
+  ```
+
+## Environment Variables
+These scripts use the following environment variables, which you can customize:
+
+- `VENV_PIP` (default: "pip"): The pip command to use.
+- `VENV_PYTHON` (default: "python3"): The Python command for creating virtual environments.
+- `VENV_DIR` (default: "$HOME/envs"): The directory where virtual environments are stored.
+
---
+++ venv.sh
@@ -0,0 +1,132 @@
+#!/bin/bash
+
+# Default values for environment variables
+: ${VENV_PIP:="pip"}
+: ${VENV_PYTHON:="python3"}
+: ${VENV_DIR:="$HOME/envs"}
+
+function venv-activate() {
+    if [[ -z "$1" ]]; then
+        echo "Usage: venv-activate <env_name>"
+        return 1
+    fi
+
+    ENV_PATH="$VENV_DIR/$1/bin/activate"
+
+    if [[ -f "$ENV_PATH" ]]; then
+        source "$ENV_PATH"
+    else
+        echo "Virtual environment not found."
+        return 1
+    fi
+}
+
+function venv-freeze() {
+    local env_name=$1
+    local env_activated=false
+    local env_was_previously_active=false
+    local env_path=""
+
+    if [[ -n "$VIRTUAL_ENV" ]]; then
+        env_path="$VIRTUAL_ENV"
+        env_name=$(basename "$VIRTUAL_ENV")
+        echo "Using currently activated environment: $env_name"
+        env_was_previously_active=true
+    elif [[ -n "$env_name" ]]; then
+        env_path="$HOME/bash/envs/$env_name"
+        venv-activate "$env_name" && env_activated=true
+    else
+        echo "Usage: venv-freeze [env_name]"
+        echo "No environment is currently activated, and no environment name was provided."
+        return 1
+    fi
+
+    if [[ $env_activated == true ]] || [[ $env_was_previously_active == true ]]; then
+        $VENV_PIP freeze > "$env_path/requirements.txt"
+        echo "Requirements for $env_name have been frozen."
+
+        if [[ $env_activated == true ]] && [[ $env_was_previously_active == false ]]; then
+            deactivate
+        fi
+    else
+        echo "Failed to activate the environment $env_name."
+        return 1
+    fi
+}
+
+
+_venv_complete() {
+    local cur="${COMP_WORDS[COMP_CWORD]}"
+    local envs=$(ls -d $VENV_DIR/*/ | xargs -n 1 basename)
+    COMPREPLY=($(compgen -W "${envs}" -- ${cur}))
+}
+
+complete -F _venv_complete venv-activate
+complete -F _venv_complete venv-freeze
+
+
+function venv-create() {
+    if [[ -z "$1" ]]; then
+        echo "Usage: venv-create <env_name>"
+        return 1
+    fi
+
+    local env_name=$1
+    local env_path="$VENV_DIR/$env_name"
+
+    if [[ -d "$env_path" ]]; then
+        echo "Virtual environment $env_name already exists."
+        return 1
+    fi
+
+    $VENV_PYTHON -m venv "$env_path"
+
+    if [[ -d "$env_path" ]]; then
+        echo "Virtual environment $env_name created."
+        venv-activate "$env_name"
+    else
+        echo "Failed to create the virtual environment $env_name."
+        return 1
+    fi
+}
+
+function venv-init() {
+    if [[ -f "$VENV_DIR/init.sh" ]]; then
+        echo "Initializing virtual environments..."
+        setup_venv() {
+            local env_dir=$1
+            local env_name=$(basename "$env_dir")
+
+            echo "Setting up virtual environment for $env_name"
+
+            # Create virtual environment if it doesn't exist
+            if [[ ! -d "$env_dir/bin" ]]; then
+                python3 -m venv "$env_dir"
+            fi
+
+            # Activate the virtual environment
+            source "$env_dir/bin/activate"
+
+            # Check if requirements.txt exists and install packages
+            if [[ -f "$env_dir/requirements.txt" ]]; then
+                pip install -r "$env_dir/requirements.txt"
+            else
+                echo "No requirements.txt found for $env_name, skipping package installation."
+            fi
+
+            # Deactivate the virtual environment
+            deactivate
+        }
+
+        # Export the setup_venv function so it can be used in subshells
+        export -f setup_venv
+
+        # Find each directory in VENV_DIR and call setup_venv on it
+        find "$VENV_DIR" -mindepth 1 -maxdepth 1 -type d ! -name '.git' -exec bash -c 'setup_venv "$0"' {} \;
+
+        echo "All virtual environments have been set up."
+    else
+        echo "init.sh script not found in $VENV_DIR"
+        return 1
+    fi
+}
